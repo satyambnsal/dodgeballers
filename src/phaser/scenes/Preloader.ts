1ecda8d4c08d77ab0a6d5783421c7fcc40f98542
yash mittal <yashmittalmm@gmail.com>
2024-04-07 23:30:26 +0530

    Game start screen and debug

--- src/phaser/scenes/Preloader.ts
+++ src/phaser/scenes/Preloader.ts
@@ -1,4 +1,4 @@
-import { Scene , Phaser} from "phaser";
+import { Scene, Phaser } from "phaser";
 
 export class Preloader extends Scene {
   constructor() {
@@ -15,19 +15,25 @@ export class Preloader extends Scene {
   }
 
   preload() {
-    this.load.image('cannon_head', 'assets/cannon_head.png');
-    this.load.image('cannon_body', 'assets/cannon_body.png');
-    this.load.image('bomb', 'assets/bomb.png');
-    this.load.image('launchBtnImg', 'assets/launchBtn.png');
-    this.load.image('launchBtnImg', 'assets/launchBtn.png');
-    this.load.image('targetImg', 'assets/target.png');
+    this.load.image("cannon_head", "assets/cannon_head.png");
+    this.load.image("cannon_body", "assets/cannon_body.png");
+    this.load.image("bomb", "assets/bomb.png");
+    this.load.image("launchBtnImg", "assets/launchBtn.png");
+    this.load.image("launchBtnImg", "assets/launchBtn.png");
+    this.load.image("targetImg", "assets/target.png");
+    this.load.image("startImg", "assets/startBtn.png");
 
-    this.load.spritesheet('boom', 'assets/explosion.png', { frameWidth: 64, frameHeight: 64, endFrame: 23 });
+    this.load.spritesheet("boom", "assets/explosion.png", {
+      frameWidth: 64,
+      frameHeight: 64,
+      endFrame: 23,
+    });
 
-    this.load.audio('fallingBomb', "./sounds/fallingBomb.mp3")
+    this.load.audio("fallingBomb", "./sounds/fallingBomb.mp3");
+    this.load.audio("explosion", "./sounds/explosion.mp3");
   }
 
   create() {
-    this.scene.start("MainMenu");
+    this.scene.start("PlayScreen");
   }
 }
