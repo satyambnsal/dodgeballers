53aa78f9081fdfc42610ecaa0edea267e1b7425a
Satyam Bansal <satyam@passageprotocol.com>
2024-04-08 09:05:23 +0530

    README changes

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,4 +1,4 @@
 export const VOYAGER_BASE_ADDRESS = "https://sepolia.voyager.online";
 export const BLOBERT_SITE_URL = "https://blobert.realms.world/";
 export const GITHUB_CONTRACTS_URL =
-  "https://github.com/satyambnsal/dodgeballers/tree/main/contracts";
+  "https://github.com/satyambnsal/dodgeballers";
